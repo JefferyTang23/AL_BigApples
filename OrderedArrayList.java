01a94b026a4b99989c868b3051ef51c0795de7bc
Jeffery Tang <78169529+JefferyTang23@users.noreply.github.com>
2021-12-10 12:25:48 -0500

    Add files via upload

---
+++ OrderedArrayList.java
@@ -0,0 +1,7 @@
+/*****************************************************
+Big Apples (Weichen Liu + Blueface, Jeffery Tang + Mathias, Lior + Toothless)
+APCS
+L02 -- Well Al B. Sorted
+2021-12-10
+time spent:  0.5 hours
+ *****************************************************/
