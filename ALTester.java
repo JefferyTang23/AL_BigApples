002eff07c91b2786886b0a5911d507ea6dc019a9
JefferyTang23 <jtang30@stuy.edu>
2021-12-13 12:23:31 -0500

    L02

--- ALTester.java
+++ ALTester.java
@@ -3,11 +3,11 @@ Big Apples (Weichen Liu + Blueface, Jeffery Tang + Mathias, Lior + Toothless)
 APCS
 L02 -- Well Al B. Sorted
 2021-12-10
-time spent:  0.5 hours
+time spent:  1.0 hours
 
 DISCO:
 - It is helpful to override the toString method to print the array list because it helps us tell if it is sorted or not.
-- We did not need to change the remove method because it does not make the list not sorted. 
+- We did not need to change the remove method because it does not make the list not sorted.
 
 QCC:
 - How else can we manipulate a list?
@@ -17,49 +17,55 @@ import java.util.ArrayList;
 
 public class ALTester {
   public ArrayList<Integer> kolf;
-  
+
   public String toString() { // Returns the list as a String
-    return kolf.toString(); 
+    return kolf.toString();
   }
-  
+
   public ALTester() { // Creates the list
-    kolf = new ArrayList<Integer>(23); 
+    kolf = new ArrayList<Integer>(23);
     for (int counter = 0; counter < 23; counter++){
       kolf.add((int)(Math.random() * 50));
     }
   }
-  
+
   public boolean isSorted() { // Checks if the list is sorted or not
     for (int i = 0; i < kolf.size(); i++) {
       if ((kolf.get(i).compareTo(kolf.get(i+1))) > 0){
-        return false; 
+        return false;
       }
     }
-    return true; 
+    return true;
   }
-  
+
   public static void main (String[] args) {
     System.out.println("\nPOPULATING AN ARRAY LIST:");
-    ALTester jeff = new ALTester(); 
-    System.out.println(jeff.toString()); // Prints the randomly populated array list 
+    ALTester jeff = new ALTester();
+    System.out.println(jeff.toString()); // Prints the randomly populated array list
     System.out.println("CHECKING IF THAT ARRAY LIST IS SORTED:");
     System.out.println(jeff.isSorted()); // Should print false most of the time
-    
-    
+
+
     System.out.println("\nTESTING OrderedArrayList:");
     OrderedArrayList hwuan = new OrderedArrayList();
-    System.out.println("TESTING add() from OrderedArrayList:");
-    hwuan.add(5); 
-    hwuan.add(6);
-    hwuan.add(1);
-    hwuan.add(23);
+    System.out.println("TESTING addLinear() from OrderedArrayList:");
+    hwuan.addLinear(5);
+    hwuan.addLinear(6);
+    hwuan.addLinear(1);
+    hwuan.addLinear(23);
     System.out.println(hwuan.toString()); // Should print [1, 5, 6, 23]
     System.out.println("TESTING get() from OrderedArrayList:");
     System.out.println(hwuan.get(2)); // Should print 6
     System.out.println("TESTING remove() from OrderedArrayList:");
     hwuan.remove(2);
     System.out.println(hwuan.toString()); // Should print [1, 5, 23]
-    
+
+    OrderedArrayList Franz = new OrderedArrayList();
+    // testing linear search
+    for( int i = 0; i < 15; i++ )
+      Franz.addLinear( (int)( 50 * Math.random() ) );
+    System.out.println("Populating another OrderedArrayList with random integers:");
+    System.out.println( Franz );
     }
 
 }
--- README.md
+++ README.md
@@ -2,13 +2,13 @@
 * Big Apples: Weichen Liu + Blueface, Jeffery Tang + Mathias, Lior Polischouk + Toothless
 
 ## ALTester
-* The constructor creates an ArrayList populated with 23 Integers. The toString method is run on the ArrayList to check if the list is sorted or not. isSorted() returns true if list is sorted and false if it is not sorted. 
-* An object of class OrderedArrayList is also created. Integers are either added or removed from the instance variable that contains the array list in the object to check for functionality. 
+* The constructor creates an ArrayList populated with 23 Integers. The toString method is run on the ArrayList to check if the list is sorted or not. isSorted() returns true if list is sorted and false if it is not sorted.
+* An object of class OrderedArrayList is also created. Integers are either added or removed from the instance variable that contains the array list in the object to check for functionality.
 
 ## OrderedArrayList
 * Constructor: creates new ArrayList<Integer> object _data
-* toString(): returns the list in String form using default toString() method
+* toString(): returns the list in String form using the default toString() method from ArrayList
 * size(): returns the size of the list using default size() method from ArrayList
 * get(): returns the element at the specified index using default get() method from ArrayList
 * remove(): removes element from list at specific index using default remove() method from ArrayList
-* add(): adds inputted element to the list depending on its value. If the inputted element has a lesser value than the element on the list, then the inputted value is added to the index of that element on the list. If the inputted element has a value greater than all of the elements on the list, it is simply added to the end of the list. A for-loop is used to compare the value of the inputted element to the elements on the list. Ex. if inputted value is 2 and the list is [0,1], the 2 would go after the 1. 
+* addLinear(): adds inputted element to the list depending on its value. If the inputted element has a smaller value than the element on the list, then the inputted value is added to the index of that element on the list. If the inputted element has a value greater than all of the elements on the list, it is simply added to the end of the list. A for-loop is used to compare the value of the inputted element to the elements on the list. Ex. if inputted value is 2 and the list is [0,1], the 2 would go after the 1.
